0a0711c46d841a5031b37d21183d9e154d851d8b
Kevin <kevin.svanback@gmail.com>
2021-12-21 11:43:34 +0100

    .

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,15 @@
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const addSelectedYear = (year) => {
+    console.log(`Coming from Expenses: ${year}`);
+  };
+
   return (<Card className='expenses'>
+    <ExpensesFilter onSelectedYear={addSelectedYear} />
     <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
     <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
     <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
