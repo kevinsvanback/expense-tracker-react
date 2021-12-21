ad0f2dc98dd2007cc72f27b30286de419c8e6559
Kevin <kevin.svanback@gmail.com>
2021-12-21 15:22:48 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_DATA = [
+  {
+    id: 'e1',
+    title: 'Padel',
+    amount: 75,
+    date: new Date(2021, 11, 15)
+  },
+  {
+    id: 'e2',
+    title: 'Beer',
+    amount: 149,
+    date: new Date(2021, 11, 17)
+  },
+  {
+    id: 'e3',
+    title: 'Board games',
+    amount: 529,
+    date: new Date(2021, 11, 27)
+  },
+  {
+    id: 'e4',
+    title: 'Champagne',
+    amount: 379,
+    date: new Date(2021, 11, 31)
+  }
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Padel',
-      amount: 75,
-      date: new Date(2021, 11, 15)
-    },
-    {
-      id: 'e2',
-      title: 'Beer',
-      amount: 149,
-      date: new Date(2021, 11, 17)
-    },
-    {
-      id: 'e3',
-      title: 'Board games',
-      amount: 529,
-      date: new Date(2021, 11, 27)
-    },
-    {
-      id: 'e4',
-      title: 'Champagne',
-      amount: 379,
-      date: new Date(2021, 11, 31)
-    }
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
 
   const addExpenseHandler = (expense) => {
     console.log('In App.js');
-    console.log(expense);
+    // setExpenses(prevState => prevState, expense);
+    setExpenses(prevExpenses => [expense, ...prevExpenses]);
   };
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,10 +14,11 @@ const Expenses = (props) => {
 
   return (<Card className='expenses'>
     <ExpensesFilter selectedYear={selectedYear} onSelectedYear={addSelectedYear} />
-    <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
+    {props.expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
+    {/* <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
     <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
     <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
-    <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
+    <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} /> */}
   </Card>);
 };
 
