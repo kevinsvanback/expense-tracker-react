a860ceb6aa629439b0bd474c92320cbf8c3b4f3e
Kevin <kevin.svanback@gmail.com>
2021-12-22 11:35:22 +0100

    .

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
@@ -12,17 +12,11 @@ const Expenses = (props) => {
     setSelectedYear(year);
   };
 
-  let expensesContent = <p>No expenses for this year.</p>;
-
   const filteredExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() === selectedYear);
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />);
-  }
 
   return (<Card className='expenses'>
     <ExpensesFilter selectedYear={selectedYear} onSelectedYear={addSelectedYear} />
-    {expensesContent}
+    <ExpensesList filteredExpenses={filteredExpenses} />
   </Card>);
 };
 
